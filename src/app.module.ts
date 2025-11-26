a877bdac334009ad7e410dfe6da054162aa8c30b
thelastgoth128 <thindwacyrus86@gmail.com>
2025-11-26 23:03:54 +0200

    feat: Initialize NestJS application with WaterPoints module and environment configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WaterPointsModule } from './water-points/water-points.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [],
+  imports: [WaterPointsModule, StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
