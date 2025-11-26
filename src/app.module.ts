906972d74fb183379514de554b3120b65d3c2e79
thelastgoth128 <thindwacyrus86@gmail.com>
2025-11-26 23:09:18 +0200

    feat: initialize NestJS application with TypeORM and define spatial entities for ADM, roads, and water points.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WaterPointsModule } from './water-points/water-points.module';
 import { StatsModule } from './stats/stats.module';
 import { RoadsModule } from './roads/roads.module';
 
 @Module({
-  imports: [WaterPointsModule, StatsModule, RoadsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false, // Set to false in production
+      }),
+      inject: [ConfigService],
+    }),
+    WaterPointsModule,
+    StatsModule,
+    RoadsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
