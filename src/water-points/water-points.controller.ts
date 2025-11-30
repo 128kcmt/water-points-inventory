1f4b912c3cc9486c592675615abfc8f55be108fc
thelastgoth128 <thindwacyrus86@gmail.com>
2025-11-30 22:47:24 +0200

    Revert "Merge branch 'main' of https://github.com/128kcmt/water-points-inventory"
    This reverts commit b1b64795d77a28898b34251b7533962002e22d4d, reversing
    changes made to 9f57247e4de57b2b69b58f2df6b7c191e9eaff6d.

--- src/water-points/water-points.controller.ts
+++ src/water-points/water-points.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { WaterPointsService } from './water-points.service';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @ApiTags('water-points')
 @Controller('api')
 export class WaterPointsController {
     constructor(private readonly waterPointsService: WaterPointsService) { }
 
-    @Get('water-points')
-    @ApiOperation({ summary: 'Get all water points' })
-    findAll() {
-        return this.waterPointsService.findAll();
+    @Get('nearest')
+    @ApiOperation({ summary: 'Get 5 nearest water points and routes' })
+    @ApiQuery({ name: 'lat', required: true, type: Number })
+    @ApiQuery({ name: 'lon', required: true, type: Number })
+    async getNearest(@Query('lat') lat: number, @Query('lon') lon: number) {
+        return this.waterPointsService.findNearest(lat, lon);
+    }
+
+    @Get('pop-in-buffer')
+    @ApiOperation({ summary: 'Get population in 5km buffer' })
+    @ApiQuery({ name: 'lat', required: true, type: Number })
+    @ApiQuery({ name: 'lon', required: true, type: Number })
+    async getPopulationInBuffer(@Query('lat') lat: number, @Query('lon') lon: number) {
+        return this.waterPointsService.getPopulationInBuffer(lat, lon);
     }
 }
